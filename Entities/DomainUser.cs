5410bd642ac90a279b1cfd97c4013f1d354f30f7
Pesia Kovitz <developer@Developers-MacBook-Pro.local>
2016-02-21 10:47:05 +0200

    stam

--- Entities/DomainUser.cs
+++ Entities/DomainUser.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.ComponentModel.DataAnnotations;
-
+//dddddddddddddddddddddddddddddddddddddddddddddddddd
 namespace Entities
 {
      [Table("T_DOMAIN_USERS")]
